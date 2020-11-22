85b68a594ae5a186580a5646573e73d0dea4dbf5
Josh Mann <jlmann72@gmail.com>
2020-11-22 09:27:59 -0500

    JavaScript squaring function codewar

--- Program.cs
+++ Program.cs
@@ -2,11 +2,31 @@
 
 namespace SquaringArgument
 {
-  class Program
-  {
-    static void Main(string[] args)
+    public class Kata
     {
-      Console.WriteLine("Welcome to C#");
+        // Write a static function "Square" here which takes a double
+        // and returns the square of that double
+        public double Square(double number)
+        {
+            double result = Math.Pow(number, 2);
+            return result;
+        }
     }
-  }
+    class Program
+    {
+        // Write a static function "Square" here which takes a double
+        // and returns the square of that double
+        public double Square(double number)
+        {
+            double result = (double)Math.Pow(number, 2);
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine(Square(4));
+        }
+    }
 }
